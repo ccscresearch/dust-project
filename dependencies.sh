5df40281a2785a623979b68ade8c0dca330e373f
nakao <nakaosensei@gmail.com>
2024-07-10 15:01:35 -0300

    moving dependencies.sh to the right place

--- dependencies.sh
+++
@@ -1,5 +0,0 @@
-pip3 install scipy
-pip3 install pandas
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
-pip3 install cleverhans
-pip3 install -U scikit-learn
